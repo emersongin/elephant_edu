72684b5b0b1d1293c0c6a34b8de4380218d7a5b6
= <=>
2022-03-29 20:52:11 -0300

    crud localidades

--- app/controllers/localidades.php
+++ app/controllers/localidades.php
@@ -5,18 +5,66 @@ include_once '../includes/functions.php';
 include_once '../repositories/localidades.php';
 
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
-    
+
+    if(isset($_GET['id'])) {
+        $id = isset($_GET['id']) ? parseId($_GET['id']) : false;
+
+        $params = array(
+            'id' => $id
+        );
+
+        echo $id ? localidadesID($params) : falha();
+        
+    } else {
+        echo localidadesTodas();
+
+    }
+
+    exit;
 }
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    
+
+    $descricao = isset($_POST['descricao']) ? parseTexto($_POST['descricao']) : false;
+    $id_setor = isset($_POST['id_setor']) ? parseId($_POST['id_setor']) : false;
+
+    $valido = $descricao && $id_setor;
+    $params = array(
+        'descricao'  => $descricao,
+        'id_setor' => $id_setor
+    );
+
+    echo $valido ? localidadesInserir($params) : falha('parametros inválido!');
+    exit;
 }
 
 if($_SERVER['REQUEST_METHOD'] == 'PUT') {
-    
+
+    $id = isset($_GET['id']) ? parseId($_GET['id']) : false;
+    $descricao = isset($_PUT['descricao']) ? parseTexto($_PUT['descricao']) : false;
+    $id_setor = isset($_PUT['id_setor']) ? parseId($_PUT['id_setor']) : false;
+
+    $valido = $id && $descricao && $id_setor;
+    $params = array(
+        'id'             => $id,
+        'descricao'      => $descricao,
+        'id_setor'     => $id_setor
+    );
+
+    echo $valido ? localidadesAtualizar($params) : falha('parametros inválido!');
+    exit;
 }
 
 if($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+
+    $id = isset($_DELETE['id']) ? parseId($_DELETE['id']) : false;
+
+    $params = array(
+        'id' => $id
+    );
+
+    echo $id ? localidadesApagar($params) : falha('parametros inválido!');
+    exit;
     
 }
 
--- app/repositories/localidades.php
+++ app/repositories/localidades.php
@@ -2,22 +2,152 @@
 
 include "../config/database/conexao.php";
 
-function localidadeTodas($params) {
+function localidadesTodas() {
+    try {
+        global $conexao;
 
+        $conexao->beginTransaction();
+
+        $sql = 
+            "SELECT 
+                l.id,
+                l.descricao as ds_localidade,
+                l.id_setor,
+                s.descricao as ds_setor
+            FROM 
+                localidades l
+            JOIN setores s ON s.id = l.id_setor";
+
+        $consulta = $conexao->prepare($sql);
+        $consulta->execute();
+    
+        $localidades = $consulta->fetchAll(PDO::FETCH_ASSOC);
+    
+        $conexao->commit();
+
+        return sucesso(count($localidades) ? $localidades : []);
+    
+    } catch(PDOException $erro) {
+        $conexao->rollback();
+
+        return falha($erro, 500);
+    }
 }
 
-function localidadeID($params) {
+function localidadesID($params) {
+    try {
+        global $conexao;
 
+        $conexao->beginTransaction();
+
+        $sql = 
+            "SELECT 
+                l.id,
+                l.descricao as ds_localidade,
+                l.id_setor,
+                s.descricao as ds_setor
+            FROM 
+                localidades l
+            JOIN setores s ON s.id = l.id_setor
+            WHERE 
+                l.id = :id";
+
+        $consulta = $conexao->prepare($sql);
+        $consulta->execute($params);
+    
+        $localidade = $consulta->fetch(PDO::FETCH_ASSOC);
+    
+        $conexao->commit();
+
+        return sucesso($localidade ? $localidade : []);
+    
+    } catch(PDOException $erro) {
+        $conexao->rollback();
+        
+        return falha($erro, 500);
+    }
 }
 
-function localidadeInserir($params) {
+function localidadesInserir($params) {
+    try {
+        global $conexao;
 
+        $conexao->beginTransaction();
+
+        $sql = 
+            "INSERT INTO localidades
+            (
+                descricao,
+                id_setor
+            ) VALUES 
+            (
+                :descricao,
+                :id_setor
+            )";
+
+        $consulta = $conexao->prepare($sql);
+        $consulta->execute($params);
+
+        $id = $conexao->lastInsertId();
+    
+        $conexao->commit();
+
+        return sucesso($id, 201);
+    
+    } catch(PDOException $erro) {
+        $conexao->rollback();
+        
+        return falha($erro, 500);
+    }
 }
 
-function localidadeAtualziar($params) {
+function localidadesAtualizar($params) {
+    try {
+        global $conexao;
 
+        $conexao->beginTransaction();
+
+        $sql = 
+            "UPDATE 
+                localidades
+            SET
+                descricao = :descricao,
+                id_setor = :id_setor
+            WHERE
+                id = :id";
+
+        $consulta = $conexao->prepare($sql);
+        $consulta->execute($params);
+    
+        $conexao->commit();
+
+        return sucesso(true);
+    
+    } catch(PDOException $erro) {
+        $conexao->rollback();
+        
+        return falha($erro, 500);
+    }
 }
 
-function localidadeApagar($params) {
+function localidadesApagar($params) {
+    try {
+        global $conexao;
 
+        $conexao->beginTransaction();
+
+        $sql = "DELETE FROM localidades WHERE id = :id";
+
+        $consulta = $conexao->prepare($sql);
+        $consulta->execute($params);
+        
+        $conexao->commit();
+
+        return sucesso(true, 204);
+    
+    } catch(PDOException $erro) {
+        $conexao->rollback();
+
+        return falha($erro, 500);
+    }
 }
