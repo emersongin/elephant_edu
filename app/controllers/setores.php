447ca64360ecf20fa4409856105509e1a85ccf5c
= <=>
2022-03-29 21:11:59 -0300

    CRUD SETORES

--- app/controllers/setores.php
+++ app/controllers/setores.php
@@ -5,19 +5,58 @@ include_once '../includes/functions.php';
 include_once '../repositories/setores.php';
 
 if($_SERVER['REQUEST_METHOD'] == 'GET') {
-    
+    if(isset($_GET['id'])) {
+        $id = isset($_GET['id']) ? parseId($_GET['id']) : false;
+
+        $params = array(
+            'id' => $id
+        );
+
+        echo $id ? setoresID($params) : falha();
+        
+    } else {
+        echo setoresTodas();
+
+    }
+
+    exit;
 }
 
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    
+    $descricao = isset($_POST['descricao']) ? parseTexto($_POST['descricao']) : false;
+
+    $valido = $descricao;
+    $params = array(
+        'descricao'  => $descricao
+    );
+
+    echo $valido ? setoresInserir($params) : falha('parametros inválido!');
+    exit;
 }
 
 if($_SERVER['REQUEST_METHOD'] == 'PUT') {
-    
+    $id = isset($_GET['id']) ? parseId($_GET['id']) : false;
+    $descricao = isset($_PUT['descricao']) ? parseTexto($_PUT['descricao']) : false;
+
+    $valido = $id && $descricao;
+    $params = array(
+        'id'        => $id,
+        'descricao' => $descricao
+    );
+
+    echo $valido ? setoresAtualizar($params) : falha('parametros inválido!');
+    exit;
 }
 
 if($_SERVER['REQUEST_METHOD'] == 'DELETE') {
-    
+    $id = isset($_DELETE['id']) ? parseId($_DELETE['id']) : false;
+
+    $params = array(
+        'id' => $id
+    );
+
+    echo $id ? setoresApagar($params) : falha('parametros inválido!');
+    exit;
 }
 
 echo falha("metodo {$_SERVER['REQUEST_METHOD']} não disponível.");
--- app/repositories/setores.php
+++ app/repositories/setores.php
@@ -2,22 +2,144 @@
 
 include "../config/database/conexao.php";
 
-function setoresTodas($params) {
+function setoresTodas() {
+    try {
+        global $conexao;
 
+        $conexao->beginTransaction();
+
+        $sql = 
+            "SELECT 
+                s.id,
+                s.descricao as ds_setor
+            FROM 
+                setores s";
+
+        $consulta = $conexao->prepare($sql);
+        $consulta->execute();
+    
+        $setores = $consulta->fetchAll(PDO::FETCH_ASSOC);
+    
+        $conexao->commit();
+
+        return sucesso(count($setores) ? $setores : []);
+    
+    } catch(PDOException $erro) {
+        $conexao->rollback();
+
+        return falha($erro, 500);
+    }
 }
 
 function setoresID($params) {
+    try {
+        global $conexao;
+
+        $conexao->beginTransaction();
+
+        $sql = 
+            "SELECT 
+                s.id,
+                s.descricao as ds_setor
+            FROM 
+                setores s
+            WHERE
+                s.id = :id";
+
+        $consulta = $conexao->prepare($sql);
+        $consulta->execute($params);
+    
+        $setor = $consulta->fetch(PDO::FETCH_ASSOC);
+    
+        $conexao->commit();
+
+        return sucesso(count($setor) ? $setor : []);
+    
+    } catch(PDOException $erro) {
+        $conexao->rollback();
+
+        return falha($erro, 500);
+    }
 
 }
 
 function setoresInserir($params) {
+    try {
+        global $conexao;
 
+        $conexao->beginTransaction();
+
+        $sql = 
+            "INSERT INTO setores
+            (
+                descricao
+            ) VALUES 
+            (
+                :descricao
+            )";
+
+        $consulta = $conexao->prepare($sql);
+        $consulta->execute($params);
+
+        $id = $conexao->lastInsertId();
+    
+        $conexao->commit();
+
+        return sucesso($id, 201);
+    
+    } catch(PDOException $erro) {
+        $conexao->rollback();
+        
+        return falha($erro, 500);
+    }
 }
 
-function setoresAtualziar($params) {
+function setoresAtualizar($params) {
+    try {
+        global $conexao;
 
+        $conexao->beginTransaction();
+
+        $sql = 
+            "UPDATE 
+                setores
+            SET
+                descricao = :descricao
+            WHERE
+                id = :id";
+
+        $consulta = $conexao->prepare($sql);
+        $consulta->execute($params);
+    
+        $conexao->commit();
+
+        return sucesso(true);
+    
+    } catch(PDOException $erro) {
+        $conexao->rollback();
+        
+        return falha($erro, 500);
+    }
 }
 
 function setoresApagar($params) {
+    try {
+        global $conexao;
 
+        $conexao->beginTransaction();
+
+        $sql = "DELETE FROM setores WHERE id = :id";
+
+        $consulta = $conexao->prepare($sql);
+        $consulta->execute($params);
+        
+        $conexao->commit();
+
+        return sucesso(true, 204);
+    
+    } catch(PDOException $erro) {
+        $conexao->rollback();
+
+        return falha($erro, 500);
+    }
 }
