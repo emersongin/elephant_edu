5f3f57057afaef6b224ed8b71ebd7f038a7a98b5
wedgles97 <wedegles@gmail.com>
2022-03-30 00:46:48 -0300

    pequenas alterações no código cadastro.php e escola.php

--- cadastro.php
+++ cadastro.php
@@ -1,104 +1,70 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-
-    <link rel="stylesheet" href="assets/css/dashboard.css">
-    <!-- bootstrap 4 -->
-    <link rel="stylesheet" href="assets/lib/bootstrap-4.0.0-dist/css/bootstrap.min.css">
-
-    <title>Cadastrar Usuário</title>
-</head>
-<body>
+<?php 
+    include_once './app/views/templates/header.php';
+?>
     <main>
-        <div class="container">
-            <div class="row">
-                <div class="col-3 bg-dark">
-                    <a id="dashboard" href="#">
-                        <div id="container-logo" class="d-flex justify-content-center">
-                            <img id="logo-img" src="assets/img/logo.png" alt="logo">
-                            <p>Dashboard</p>
-                        </div>
-                    </a>
-
-                    <div id="container-menu">
-                      <ul>
-                          <li><a href="#" class="btn">Inicio</a></li>
-                          <li><a href="#" class="btn">Cadastrar</a></li>
-                          <li><a href="#" class="btn">Visitas</a></li>
-                          <li><a href="#" class="btn">Relatório</a></li>
-                      </ul>
-                    </div>
-                </div>
-
-                <div class="col-9">
-                    <div>
-                        <h1>Cadastrar Usuário</h1>
-                        <div class="card">
-                            <div class="card-body">
-                                <form class="row g-3">
-                                    <div class="col-md-5">
-                                        <label for="inputName4" class="form-label">Nome</label>
-                                        <input type="text" class="form-control" id="inputName4">
-                                    </div>
-                                    <div class="col-md-5">
-                                        <label for="inputLastName4" class="form-label">Sobrenome</label>
-                                        <input type="text" class="form-control" id="inputLastName4">
-                                    </div>
-                                    <div class="col-md-5">
-                                        <label for="inputEmail4" class="form-label">Email</label>
-                                        <input type="email" class="form-control" id="inputEmail4">
-                                    </div>
-                                    <div class="col-md-5">
-                                        <label for="inputEmailConfirm4" class="form-label">Confirmar Email</label>
-                                        <input type="email" class="form-control" id="inputEmailConfirm4">
-                                    </div>
-                                    <div class="col-md-5">
-                                        <label for="inputPassword4" class="form-label">Senha</label>
-                                        <input type="password" class="form-control" id="inputPassword4">
-                                    </div>
-                                    <div class="col-md-5">
-                                        <label for="inputPasswordConfirm4" class="form-label">Confirmar Senha</label>
-                                        <input type="password" class="form-control" id="inputPasswordConfirm4">
-                                    </div>
-                                    <div class="col-md-4">
-                                        <label for="inputCPF" class="form-label">CPF</label>
-                                        <input type="text" class="form-control" id="inputCPF" placeholder="123.456.789-00" maxlength="11">
-                                    </div>
-                                    <div class="col-md-3">
-                                        <label for="inputTel" class="form-label">Telefone</label>
-                                        <input type="text" class="form-control" id="inputTel" placeholder="11 4002-8922">
-                                    </div>
-                                    <div class="col-md-3">
-                                        <label for="inputPhone" class="form-label">Celular</label>
-                                        <input type="text" class="form-control" id="inputPhone" placeholder="81 99999-6666">
-                                    </div>
-                                    <div class="col-md-3">
-                                        <select id="inputSelect" class="form-select">
-                                            <option selected>Selecione</option>
-                                            <option value="1">Coordenador (a)</option>
-                                        </select>
-                                    </div>
-                                    <div class="col-12">
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" id="gridCheck">
-                                            <label class="form-check-label" for="gridCheck">Confirmar cadastro</label>
-                                        </div>
-                                    </div>
-                                    <div class="col-12">
-                                        <button type="submit" class="btn btn-primary">Cadastrar</button>
-                                    </div>
-                                </form>
+        <div class="col-12">
+            <div>
+                <h1>Cadastrar Usuário</h1>
+                <div class="card">
+                    <div class="card-body">
+                        <form class="row g-3">
+                            <div class="col-md-5">
+                                <label for="inputName4" class="form-label">Nome</label>
+                                <input type="text" class="form-control" id="inputName4">
                             </div>
-                        </div>  
+                            <div class="col-md-5">
+                                <label for="inputLastName4" class="form-label">Sobrenome</label>
+                                <input type="text" class="form-control" id="inputLastName4">
+                            </div>
+                            <div class="col-md-5">
+                                <label for="inputEmail4" class="form-label">Email</label>
+                                <input type="email" class="form-control" id="inputEmail4">
+                            </div>
+                            <div class="col-md-5">
+                                <label for="inputEmailConfirm4" class="form-label">Confirmar Email</label>
+                                <input type="email" class="form-control" id="inputEmailConfirm4">
+                            </div>
+                            <div class="col-md-5">
+                                <label for="inputPassword4" class="form-label">Senha</label>
+                                <input type="password" class="form-control" id="inputPassword4">
+                            </div>
+                            <div class="col-md-5">
+                                <label for="inputPasswordConfirm4" class="form-label">Confirmar Senha</label>
+                                <input type="password" class="form-control" id="inputPasswordConfirm4">
+                            </div>
+                            <div class="col-md-4">
+                                <label for="inputCPF" class="form-label">CPF</label>
+                                <input type="text" class="form-control" id="inputCPF" placeholder="123.456.789-00" maxlength="11">
+                            </div>
+                            <div class="col-md-3">
+                                <label for="inputTel" class="form-label">Telefone</label>
+                                <input type="text" class="form-control" id="inputTel" placeholder="11 4002-8922">
+                            </div>
+                            <div class="col-md-3">
+                                <label for="inputPhone" class="form-label">Celular</label>
+                                <input type="text" class="form-control" id="inputPhone" placeholder="81 99999-6666">
+                            </div>
+                            <div class="col-md-3">
+                                <select id="inputSelect" class="form-select">
+                                    <option selected>Selecione</option>
+                                    <option value="1">Coordenador (a)</option>
+                                </select>
+                            </div>
+                            <div class="col-12">
+                                <div class="form-check">
+                                    <input class="form-check-input" type="checkbox" id="gridCheck">
+                                    <label class="form-check-label" for="gridCheck">Confirmar cadastro</label>
+                                </div>
+                            </div>
+                            <div class="col-12">
+                                <button type="submit" class="btn btn-primary">Cadastrar</button>
+                            </div>
+                        </form>
                     </div>
                 </div>
             </div>
         </div>
     </main>
-
-    <script src="assets/lib/bootstrap-4.0.0-dist/js/bootstrap.min.js"></script>
-</body>
-</html>
+<?php 
+    include_once './app/views/templates/footer.php';  
+?>
--- escola.php
+++ escola.php
@@ -1,118 +1,75 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport"content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
-
-        <link rel="icon" type="image/png" sizes="16x16" href="assets/img/logoFavicon.png">
-
-        <title>Elephant EDU:: Cadastrar Escola</title>
-    </head>
-
-    <body>
+<?php 
+    include_once './app/views/templates/header.php';
+?>
 
         <main>
-          <div class="container" style="height: 100vh;">
-
-            <div class="row">
-
-              <div class="col-3" style="background-color: #153575">
-                  <a id="dashboard" href="#">
-                      <div id="container-logo" class="d-flex justify-content-center" style="height: 8vh; margin-top: 1.25rem;">
-                          <img id="img-fluid" src="assets/img/logoDashboard.svg" alt="logo">
-                      </div>
-                  </a>
-
-                  <div style="height: 3px; margin-top: 15px; margin-bottom: 15px;">
-                    <hr>
-                  </div>
-                  <div id="container-menu">
-                    <ul>
-                        <li><a href="#" class="btn">Inicio</a></li>
-                        <li><a href="#" class="btn">Cadastro</a></li>
-                        <li><a href="#" class="btn">Visitas</a></li>
-                        <li><a href="#" class="btn">Relatório</a></li>
-                    </ul>
-                  </div>
-              </div>
-
-              <div class="col-9">
-
+            <div class="col-12">
                 <div class="d-flex justify-content-center align-items-center h-100">
-
-                  <div class="card " style="width: 700px; height:700px; border: 3px solid #9Db4d8; border-radius: 10px;">
-                    
-                    <div class="text-center">
-                        <img src="assets/img/escola.png" class="rounded" alt="photo school" style="width: 193px; height: 166px;">
-                        <h1 style="color: #153575; margin: 20px;"> Cadastrar Escola </h1> 
-                    </div>  
+                    <div class="card " style="width: 700px; height:700px; border: 3px solid #9Db4d8; border-radius: 10px;">
+                        <div class="text-center">
+                            <img src="assets/img/escola.png" class="rounded" alt="photo school" style="width: 193px; height: 166px;">
+                            <h1 style="color: #153575; margin: 20px;"> Cadastrar Escola </h1> 
+                        </div>  
                   
-                    <form> <!--  Início do Form -->
+                        <form> <!--  Início do Form -->
 
-                      <div class="form-row d-flex justify-content-center">
-                        <div class="col-md-8">
-                          <label for="nameSchool" style="margin: 0 5px; color: #153575;">Nome da Escola</label>
-                          <input type="text" class="form-control" id="nameSchool" style="border: 2px solid #153575;" required>
-                        </div>
-                      </div>
+                            <div class="form-row d-flex justify-content-center">
+                                <div class="col-md-8">
+                                    <label for="nameSchool" style="margin: 0 5px; color: #153575;">Nome da Escola</label>
+                                    <input type="text" class="form-control" id="nameSchool" style="border: 2px solid #153575;" required>
+                                </div>
+                            </div>
 
-                      <div class="form-row d-flex justify-content-center" >
-                        <div class="col-md-7">
-                          <label for="validationAddress" style="margin: 0 5px; color: #153575;">Endereço</label>
-                          <input type="text" class="form-control" id="validationAddress" style="border: 2px solid #153575;" required>
-                        </div>
+                            <div class="form-row d-flex justify-content-center" >
+                                <div class="col-md-7">
+                                    <label for="validationAddress" style="margin: 0 5px; color: #153575;">Endereço</label>
+                                    <input type="text" class="form-control" id="validationAddress" style="border: 2px solid #153575;" required>
+                                </div>
+                                <div class="col-md-1">
+                                    <label for="validationAddressN" style="margin: 0 5px; color: #153575;">N</label>
+                                    <input type="text" class="form-control" id="validationAddressN" style="border: 2px solid #153575;" required>
+                                </div>
+                            </div>
 
-                        <div class="col-md-1">
-                          <label for="validationAddressN" style="margin: 0 5px; color: #153575;">N</label>
-                          <input type="text" class="form-control" id="validationAddressN" style="border: 2px solid #153575;" required>
-                        </div>
-                      </div>
+                            <div class="form-row d-flex justify-content-center">
+                                <div class="form-group col-md-4">
+                                    <label for="inputCity" style="margin: 0 5px; color: #153575;">Localidade</label>
+                                    <input type="text" class="form-control" id="inputCity" style="border: 2px solid #153575;">
+                                </div>
+                                <div class="form-group col-md-4 ">
+                                    <label for="inputState" style="margin: 0 5px; color: #153575;">Bairro</label>
+                                    <select id="inputState" class="form-control">
+                                        <option selected >Selecione o bairro</html:option>
+                                        <option value="1">Universitário</option>
+                                        <option value="2">Maurício de Nassau</option>
+                                        <option value="3">Rendeiras</option>
+                                        <option value="4">Salgado</option>
+                                        <option value="5">Divinópolis</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div class="form-row d-flex justify-content-center">
+                                <div class="form-group col-md-6">
+                                    <label for="inputState" style="margin: 0 5px; color: #153575;" >Responsável</label>
+                                    <select id="inputState" class="form-control">
+                                        <option selected>Selecione uma opção</option>
+                                        <option value="1">Maria</option>
+                                        <option value="2">José</option>
+                                        <option value="3">Carolina</option>
+                                        <option value="4">Pedro</option>
+                                        <option value="5">Isadora</option>
+                                    </select>
+                                </div>
+                            </div>
 
-                      <div class="form-row d-flex justify-content-center">
-                        <div class="form-group col-md-4">
-                          <label for="inputCity" style="margin: 0 5px; color: #153575;">Localidade</label>
-                          <input type="text" class="form-control" id="inputCity" style="border: 2px solid #153575;">
-                        </div>
-
-                        <div class="form-group col-md-4 ">
-                          <label for="inputState" style="margin: 0 5px; color: #153575;">Bairro</label>
-                          <select id="inputState" class="form-control">
-                              <option selected >Selecione o bairro</html:option>
-                              <option value="1">Universitário</option>
-                              <option value="2">Maurício de Nassau</option>
-                              <option value="3">Rendeiras</option>
-                              <option value="4">Salgado</option>
-                              <option value="5">Divinópolis</option>
-                          </select>
-                        </div>
-                      </div>
-
-                      <div class="form-row d-flex justify-content-center">
-                        <div class="form-group col-md-6">
-                          <label for="inputState" style="margin: 0 5px; color: #153575;" >Responsável</label>
-                          <select id="inputState" class="form-control">
-                            <option selected>Selecione uma opção</option>
-                            <option value="1">Maria</option>
-                            <option value="2">José</option>
-                            <option value="3">Carolina</option>
-                            <option value="4">Pedro</option>
-                            <option value="5">Isadora</option>
-                          </select>
-                        </div>
-                      </div>
-
-                      <div class="d-flex justify-content-center">
-                        <button type="button" class="btn btn-outline-primary">Cadastrar</button>
-                      </div>
-                    </form> <!--  Final do Form -->
-                  </div>
+                            <div class="d-flex justify-content-center">
+                                  <button type="button" class="btn btn-outline-primary">Cadastrar</button>
+                            </div>
+                        </form> <!--  Final do Form -->
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
         </main>
-    </body>
-</html>
+<?php 
+    include_once './app/views/templates/footer.php';
+?>
